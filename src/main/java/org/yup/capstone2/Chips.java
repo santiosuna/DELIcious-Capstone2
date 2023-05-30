c9740b1a51684cdc653bbc504ed51a77b09a2857
Jhon Laguerre <icondon@protonmail.com>
2023-05-30 13:26:53 -0400

    enum in progress

---
+++ src/main/java/org/yup/capstone2/Chips.java
@@ -0,0 +1,9 @@
+package org.yup.capstone2;
+
+public enum Chips {
+
+    RANCHDORITOS,
+    HOTFRIES,
+    RUFFLES,
+    SPICYNACHODORITOS
+}
---
+++ src/main/java/org/yup/capstone2/Drinks.java
@@ -0,0 +1,16 @@
+package org.yup.capstone2;
+
+public enum Drinks {
+    PINEAPPLEMANGO,
+    ORANGE,
+    RASPBERRY,
+    LEMONWATER;
+
+    private String size;
+    private String flavor;
+    private double price;
+
+
+    }
+
+
