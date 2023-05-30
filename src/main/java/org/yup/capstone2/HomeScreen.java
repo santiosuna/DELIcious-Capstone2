c9921113a3c6acc7a80d71dc427fca22e7628aac
Santiago Osuna <santiago_osuna@icloud.com>
2023-05-30 09:49:25 -0400

    finished HomeScreen class

---
+++ src/main/java/org/yup/capstone2/HomeScreen.java
@@ -0,0 +1,30 @@
+package org.yup.capstone2;
+import java.util.Scanner;
+class HomeScreen {
+    public static void homeScreenDisplay() {
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("                ◩ HOME SCREEN ◪                 ");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("Pleas make a selection:");
+        System.out.println("1) New Order");
+        System.out.println("0) Exit");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("Enter your selection here:");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                // Code to handle new order
+                break;
+            case 0:
+                System.out.println("\n◢◤◢◤◢◤◢◤◢◤◢◤◢◤ Exiting App. Goodbye! ◢◤◢◤◢◤◢◤◢◤◢◤◢◤\n");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again!");
+        }
+    }
+}
+
