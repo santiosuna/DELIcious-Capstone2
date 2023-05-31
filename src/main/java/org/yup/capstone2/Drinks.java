94716e005062597ec6d0eed3ce9b79ff3429627f
Jhon Laguerre <icondon@protonmail.com>
2023-05-31 15:39:22 -0400

    created drinksClass

---
+++ src/main/java/org/yup/capstone2/Drinks.java
@@ -0,0 +1,38 @@
+package org.yup.capstone2;
+
+public class Drinks {
+
+    private String name;
+    private double price;
+    private String size;
+
+    public Drinks(String name, double price, String size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
